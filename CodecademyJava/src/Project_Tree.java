887015cbc1d6350686ec018d8090879907d51455
alxmart <alxmartinez@gmail.com>
2023-01-31 13:21:49 -0300

    Added more course exercises.

---
+++ CodecademyJava/src/Project_Tree.java
@@ -0,0 +1,19 @@
+public class Project_Tree {
+
+    public static void main(String [] args) {
+
+    /*
+    We’re going to introduce ourselves and print a tree to the screen.
+    */
+        System.out.println("Hey there, I am Alex and I am learning to code in Java");
+        System.out.println("“I’m going to plant a tree today!”");
+        System.out.println("    *  ");
+        System.out.println("   ***  ");
+        System.out.println("  ****** ");
+        System.out.println(" ******** ");
+        System.out.println("    *  ");
+        System.out.println("    *  ");
+
+    }
+
+}
