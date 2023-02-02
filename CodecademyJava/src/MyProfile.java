8780f8909f144f67b44994e12ab76fb52da9e85b
alxmart <alxmartinez@gmail.com>
2023-02-02 10:55:41 -0300

    Added more course exercises.

---
+++ CodecademyJava/src/MyProfile.java
@@ -0,0 +1,13 @@
+public class MyProfile {
+    public static void main(String[] args) {
+
+        String name = "Alex";
+        int age = 52;
+        double desiredSalary = 60000.00; // US$ year
+        char gender = 'M';
+        boolean lookingForJob = true;
+        System.out.println(name);
+
+
+    }
+}
