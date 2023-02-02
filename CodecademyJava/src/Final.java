cf21ce2b24c1eed3c699073840f0049447e856c7
alxmart <alxmartinez@gmail.com>
2023-02-02 12:05:44 -0300

    Added more course exercises.

---
+++ CodecademyJava/src/Final.java
@@ -0,0 +1,12 @@
+public class Final {
+    public static void main(String[] args) {
+
+        final double pi = 3.14;
+
+        //pi = 77.89; // Cannot assign a value to final variable pi
+
+        System.out.println(pi);
+
+
+    }
+}
