12e0e80602263e5fcf6e75b3448708d2fcba3cb1
alxmart <alxmartinez@gmail.com>
2023-02-22 10:45:43 -0300

    Added more course exercises.

---
+++ CodecademyJava/src/Car.java
@@ -0,0 +1,19 @@
+class Car {
+
+    String color;
+
+    public Car(String carColor) {
+        color = carColor;
+    }
+
+    public void startRadio(double stationNum, String stationName) {
+        System.out.println("Turning on the radio to " + stationNum + ", " + stationName + "!");
+        System.out.println("Enjoy!");
+    }
+
+    public static void main(String[] args) {
+        Car myCar = new Car("red");
+        myCar.startRadio(103.7, "Meditation Station");
+    }
+
+}
