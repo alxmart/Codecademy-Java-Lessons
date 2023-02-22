4ce8c8b9a5e008d53b707d3152fc62c1baa60bb1
alxmart <alxmartinez@gmail.com>
2023-02-22 09:59:16 -0300

    Added more course exercises.

---
+++ CodecademyJava/src/Store2.java
@@ -0,0 +1,28 @@
+public class Store2 {
+
+    // instance fields
+    String productType;
+
+    // constructor method
+    public Store2(String product) {
+        productType = product;
+    }
+
+    // advertise method
+    public void advertise2() {
+        System.out.println("Selling " + productType + "!");
+        System.out.println("Come spend some money!");
+    }
+
+    // main method
+    public static void main(String[] args) {
+
+        Store2 lemonadeStand2 = new Store2("Lemonade");
+
+        lemonadeStand2.advertise2();
+        lemonadeStand2.advertise2();
+        lemonadeStand2.advertise2();
+
+
+    }
+}
