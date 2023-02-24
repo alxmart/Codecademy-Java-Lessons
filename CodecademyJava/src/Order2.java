a75b072013584a81be723d6e41c4088605b63b3c
alxmart <alxmartinez@gmail.com>
2023-02-24 18:43:59 -0300

    Added more course exercises.

---
+++ CodecademyJava/src/Order2.java
@@ -0,0 +1,41 @@
+public class Order2 {
+    boolean isFilled;
+    double billAmount;
+    String shipping;
+
+    public Order2(boolean filled, double cost, String shippingMethod) {
+
+        if (cost > 24.00) {
+            System.out.println("High value item!");
+        }
+
+        isFilled = filled;
+        billAmount = cost;
+        shipping = shippingMethod;
+    }
+
+    public void ship() {
+        if (isFilled) {
+            System.out.println("Shipping");
+            System.out.println("Shipping cost: " + calculateShipping());
+        } else {
+            System.out.println("Order not ready");
+        }
+    }
+
+    public double calculateShipping() {
+        // declare conditional statement here
+
+        return 0;
+
+    }
+
+    public static void main(String[] args) {
+        // do not alter the main method!
+        Order2 book = new Order2(true, 9.99, "Express");
+        Order2 chemistrySet = new Order2(false, 72.50, "Regular");
+
+        book.ship();
+        chemistrySet.ship();
+    }
+}
