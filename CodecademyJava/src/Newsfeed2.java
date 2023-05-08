436adaaec1e92e29b8f0ff42b4d5a936e5359878
alxmart <alxmartinez@gmail.com>
2023-05-08 11:47:03 -0300

    Added more course exercises and projects.

--- CodecademyJava/src/Newsfeed2.java
+++ CodecademyJava/src/Newsfeed2.java
@@ -1,25 +1,40 @@
+import java.util.Arrays;
+
 public class Newsfeed2 {
 
+    String[] topics = {"Opinion", "Tech", "Science", "Health"};
+    int[] views = {0, 0, 0, 0};
 
     public Newsfeed2(){
 
     }
 
-    // Create getTopics() below:
-    public String[] getTopics() {
-
-        String[] topics= {"Opinion","Tech","Science","Health"};
+    public String[] getTopics(){
         return topics;
     }
 
+    public String getTopTopic(){
+
+        return topics[0];
+    }
+
+    public void viewTopic(int topicIndex){
+        views[topicIndex] +=1;
+
+    }
 
     public static void main(String[] args){
-
         Newsfeed2 sampleFeed = new Newsfeed2();
 
-        String[] topics = sampleFeed.getTopics();
+        System.out.println("The top topic is "+ sampleFeed.getTopTopic());
 
-        System.out.println(topics);
+        sampleFeed.viewTopic(1);
+        sampleFeed.viewTopic(1);
+        sampleFeed.viewTopic(3);
+        sampleFeed.viewTopic(2);
+        sampleFeed.viewTopic(2);
+        sampleFeed.viewTopic(1);
 
+        System.out.println("The " + sampleFeed.topics[1] + " topic has been viewed " + sampleFeed.views[1] + " times!");
     }
 }
