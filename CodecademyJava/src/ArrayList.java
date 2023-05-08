ad740e80ce4c5f8baace3349b41b6a8ad32e95c0
alxmart <alxmartinez@gmail.com>
2023-05-08 13:53:36 -0300

    Added more course exercises and projects.

---
+++ CodecademyJava/src/ArrayList.java
@@ -0,0 +1,18 @@
+/*
+
+// Declaring:
+-------------------------------------------------------------
+
+   ArrayList<Integer> ages;
+
+// Initializing:
+-------------------------------------------------------------
+
+   ages = new ArrayList<Integer>();
+
+// Declaring and initializing in one line:
+--------------------------------------------------------------
+
+   ArrayList<String> babyNames = new ArrayList<String>();
+
+ */
