2bfc42aff2276dc5aa3d7ee213c55cee9f02bad7
alxmart <alxmartinez@gmail.com>
2023-05-08 15:17:17 -0300

    Added more course exercises and projects.

---
+++ CodecademyJava/src/Project_Desert_Island_Playlist.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class Project_Desert_Island_Playlist {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
+
+        desertIslandPlaylist.add("Europe - The Final Countdown");
+        desertIslandPlaylist.add("Black Sabbath - Dying for love");
+        desertIslandPlaylist.add("Yngwie Malmsteen - Black Star");
+        desertIslandPlaylist.add("Black Sabbath - Computer God");
+        desertIslandPlaylist.add("Laura Poldvere - Moonwalk");
+        desertIslandPlaylist.add("Laura Poldvere - Supersonic");
+
+        System.out.println(desertIslandPlaylist);
+        System.out.println(desertIslandPlaylist.size());
+
+        desertIslandPlaylist.remove("Black Sabbath - Dying for love");
+
+        System.out.println(desertIslandPlaylist);
+
+        //Swap:
+        int indexA = desertIslandPlaylist.indexOf("Laura Poldvere - Moonwalk");
+
+        int indexB = desertIslandPlaylist.indexOf("Laura Poldvere - Supersonic");
+
+        String tempA = "Laura Poldvere - Moonwalk";
+
+        desertIslandPlaylist.set(indexA, "Laura Poldvere - Supersonic");
+
+        System.out.println(desertIslandPlaylist);
+
+        desertIslandPlaylist.set(indexB, tempA);
+
+        System.out.println(desertIslandPlaylist);
+
+    }
+}
