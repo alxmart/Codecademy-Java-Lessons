2d1b3de3876489b05baaff67c2bd4914179f78c7
alxmart <alxmartinez@gmail.com>
2023-05-13 10:03:12 -0300

    Adding more tests, exercises and projects according to lessons.

---
+++ CodecademyJava/src/Debugging/Debug2.java
@@ -0,0 +1,15 @@
+package Debugging;
+
+public class Debug2 {
+
+    public static void main(String[] args) {
+
+        int width = 20;
+        int length = 40;
+
+        int ratio = length / width;
+
+        System.out.println(ratio);
+
+    }
+}
