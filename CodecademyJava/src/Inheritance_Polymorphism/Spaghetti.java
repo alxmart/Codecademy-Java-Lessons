def82a63729dc10fc89b2fa4b6f39753df57bb51
alxmart <alxmartinez@gmail.com>
2023-05-13 12:21:59 -0300

    Adding more tests, exercises and projects according to lessons.

---
+++ CodecademyJava/src/Inheritance_Polymorphism/Spaghetti.java
@@ -0,0 +1,9 @@
+package Inheritance_Polymorphism;
+
+class Spaghetti  extends Noodle {
+
+
+    Spaghetti(double lenInCent, double wthInCent, String shp, String ingr) {
+        super(lenInCent, wthInCent, shp, ingr);
+    }
+}
