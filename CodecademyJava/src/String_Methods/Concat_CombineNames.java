7e8236c56a070aae74758578e2f8dbdfe177527c
alxmart <alxmartinez@gmail.com>
2023-05-13 08:39:09 -0300

    Adding more tests, exercises and projects according to lessons.

---
+++ CodecademyJava/src/String_Methods/Concat_CombineNames.java
@@ -0,0 +1,15 @@
+package String_Methods;
+
+public class Concat_CombineNames {
+
+    public static void main(String[] args) {
+
+        // Add your first names:
+        String firstName = "Luiz ";
+        String lastName = "Martinez";
+
+        // What's the full name?
+        System.out.println(firstName.concat(lastName));
+    }
+}
+
---
+++ CodecademyJava/src/String_Methods/Lenght_Tweet.java
@@ -0,0 +1,14 @@
+package String_Methods;
+
+public class Lenght_Tweet {
+
+    public static void main(String[] args) {
+
+        String tweet = "Liz Lemon, ninjas are kind of cool... I just dont know any personally. Get on that.";
+
+        // What's the character count?
+        System.out.println(tweet.length());
+
+    }
+
+}
