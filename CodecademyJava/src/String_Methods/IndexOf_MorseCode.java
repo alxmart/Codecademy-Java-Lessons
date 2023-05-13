a9761018a9d943dc8195eb002b35f674bd7583b3
alxmart <alxmartinez@gmail.com>
2023-05-13 08:50:23 -0300

    Adding more tests, exercises and projects according to lessons.

---
+++ CodecademyJava/src/String_Methods/IndexOf_MorseCode.java
@@ -0,0 +1,22 @@
+package String_Methods;
+
+public class IndexOf_MorseCode {
+
+//    String letters = "ABCDEFGHIJKLMN";
+//    System.out.println(letters.indexOf("C"));
+//    This would output 2.
+//
+//    String letters = "ABCDEFGHIJKLMN";
+//    System.out.println(letters.indexOf("EFG"));
+//    This would output 4, because EFG starts at index 4.
+
+    public static void main(String[] args) {
+
+        String code = ".... .. / .. .----. -- / ... --- -. -. -.-- / .- -. -.. / .. / .-.. .. -.- . / - --- / . .- - / .-. .- -- . -.";
+
+        // Write the code:
+        System.out.println(code.indexOf(". .- -"));
+    }
+
+
+}
