f26e6efd6ec6b5a94608e79f06509d5bfc5df0cd
alxmart <alxmartinez@gmail.com>
2023-05-13 13:10:27 -0300

    Adding more tests, exercises and projects according to lessons.

---
+++ CodecademyJava/src/Inheritance_Polymorphism/Noodle4.java
@@ -0,0 +1,24 @@
+package Inheritance_Polymorphism;
+
+class Noodle4 {
+
+    protected double lengthInCentimeters;
+    protected double widthInCentimeters;
+    protected String shape;
+    protected String ingredients;
+    protected String texture = "brittle";
+
+    Noodle4(double lenInCent, double wthInCent, String shp,
+            String ingr) {
+
+        this.lengthInCentimeters = lenInCent;
+        this.widthInCentimeters = wthInCent;
+        this.shape = shp;
+        this.ingredients = ingr;
+    }
+
+    public void cook() {
+        this.texture = "cooked";
+    }
+
+}
