0eeb87cbb193ffeeec73a853dcb62df1e4887a25
alxmart <alxmartinez@gmail.com>
2023-05-13 08:54:06 -0300

    Adding more tests, exercises and projects according to lessons.

---
+++ CodecademyJava/src/String_Methods/CharAt_Initials.java
@@ -0,0 +1,20 @@
+package String_Methods;
+
+public class CharAt_Initials {
+
+//String str = "qwer";
+//System.out.println(str.charAt(2));
+//It would output e because that’s what’s at index 2.
+
+    public static void main(String[] args) {
+
+        // Add a first name and a last name:
+        String firstName = "Luiz";
+        String lastName = "Martinez";
+
+        // What are the initials?
+        System.out.println(firstName.charAt(0));
+        System.out.println(lastName.charAt(0));
+
+    }
+}
