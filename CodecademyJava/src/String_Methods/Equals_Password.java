637478e66bbaf6d38cad00bd999ede3e1ff36e4e
alxmart <alxmartinez@gmail.com>
2023-05-13 08:45:35 -0300

    Adding more tests, exercises and projects according to lessons.

---
+++ CodecademyJava/src/String_Methods/Equals_Password.java
@@ -0,0 +1,26 @@
+package String_Methods;
+
+public class Equals_Password {
+
+//    String flavor1 = "Mango";
+//    String flavor2 = "Peach";
+//    System.out.println(flavor1.equals("Mango"));
+//    // prints true
+//    System.out.println(flavor2.equals("Mango"));
+//    // prints false
+
+//    String flavor1 = "Mango";
+//    String flavor2 = "Peach";
+//    System.out.println(flavor1.compareTo(flavor2));
+//In the example above, "Mango" comes before "Peach",
+//    so we get a negative number (we specifically get -3
+//    because the Unicode values of "M" and "P" differ by 3).
+
+    public static void main(String[] args) {
+
+        String password = "correcthorsebatterystaple";
+        // Write the code:
+        System.out.println(password.equals("Space Station"));
+    }
+
+}
