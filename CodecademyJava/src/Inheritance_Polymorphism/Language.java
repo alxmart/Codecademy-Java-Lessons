b821357baf5331319eeecdd5d5ad8a43e0ae1145
alxmart <alxmartinez@gmail.com>
2023-05-14 10:01:48 -0300

    Adding more tests, exercises and projects according to lessons.

---
+++ CodecademyJava/src/Inheritance_Polymorphism/Language.java
@@ -0,0 +1,41 @@
+package Inheritance_Polymorphism;
+
+class Language {
+
+    protected String name;
+    protected int numSpeakers;
+    protected String regionsSpoken;
+    protected String wordOrder;
+
+    Language(
+            String langName,
+            int speakers,
+            String regions,
+            String wdOrder) {
+        this.name = langName;
+        this.numSpeakers = speakers;
+        this.regionsSpoken = regions;
+        this.wordOrder = wdOrder;
+    }
+
+    public void getInfo() {
+
+        System.out.println(this.name + " is spoken by "
+                + this.numSpeakers + " people mainly in "
+                + this.regionsSpoken + ".");
+
+        System.out.println("The language follows the word order: "
+                + this.wordOrder);
+    }
+
+    public static void main(String[] args) {
+
+        Language english = new Language("br portuguese",
+                230000000,
+                "2",
+                "5");
+
+        english.getInfo();
+
+    }
+}
---
+++ CodecademyJava/src/Inheritance_Polymorphism/Mayan.java
@@ -0,0 +1,4 @@
+package Inheritance_Polymorphism;
+
+public class Mayan {
+}
